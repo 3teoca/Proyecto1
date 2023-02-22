30b31b60e99d480607f83fadbae2b05d36626430
3teoca <3teoca@gmail.com>
2023-02-22 11:23:45 -0500

    Login y registro

--- app.js
+++ app.js
@@ -8,5 +8,13 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname,"./views/home.html"))
     })
 
+app.get('/login.html', function(req,res) {
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+    })
 
-app.listen(3000, ()=> console.log("funcionando en el puerto 3000"))
+app.get('/registro.html', function(req,res) {
+    res.sendFile(path.join(__dirname,"./views/registro.html"))
+    })
+
+
+app.listen(3000, ()=> console.log("Funcionando en el puerto 3000"))
